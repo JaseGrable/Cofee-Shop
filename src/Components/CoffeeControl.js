fc0ccc3d7d2af7893223991a22fbe6f2bb70b4e8
JaseGrable <jase.grable@gmail.com>
2023-09-18 08:26:02 -0700

    Update prop type

--- src/Components/CoffeeControl.js
+++ src/Components/CoffeeControl.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Header from './Header';
 import NewCoffeeForm from './NewCoffeeForm';
 import CoffeeList from './CoffeeList';
 import CoffeeDetail from './CoffeeDetail';
 import EditCoffeeForm from './EditCoffeeForm';
-
+import Header from './Header';
 import { v4 } from 'uuid';
 
 class CoffeeControl extends React.Component {
@@ -17,166 +16,166 @@ class CoffeeControl extends React.Component {
         editing: false,
         mainCoffeeList: [
             {
-                name: "Dark Roast 12oz Ground* For Coffee Maker",
-                origin: "Guatemala",
-                description: "Coffee Directly sourced from the Mountains of El Bosque, Santa Cruz Naranjo, Santa Rosa, Guatemala.",
-                roast: "Dark",
-                price: 8.99,
-                available: 130,
-                id: v4()
+              name: "Morning Bliss",
+              origin: "Colombia",
+              description: "A medium roast coffee from Colombia that delivers a smooth and energizing start to your day.",
+              roast: "Medium",
+              price: 8.99,
+              available: 130,
+              id: v4()
             },
             {
-                name: "Dark Roast 12oz Whole Beans",
-                origin: "Guatemala",
-                description:"Coffee Directly sourced from the Mountains of El Bosque, Santa Cruz Naranjo, Santa Rosa, Guatemala.",
-                roast: "Dark",
-                price: 9.99,
-                available: 90,
-                id: v4()
+              name: "Espresso Delight",
+              origin: "Italy",
+              description:"Indulge in the bold and rich flavors of Italy with this dark roast espresso that's perfect for espresso lovers.",
+              roast: "Dark",
+              price: 9.99,
+              available: 90,
+              id: v4()
             },
             {
-                name: "Medium Roast 12oz Ground* For Coffee Maker",
-                origin: "Guatemala",
-                description: "Coffee Directly sourced from the Mountains of El Bosque, Santa Cruz Naranjo, Santa Rosa, Guatemala",
-                roast: "Medium",
-                price: 7.99,
-                available: 50,
-                id: v4()
+              name: "Tropical Sunrise",
+              origin: "Costa Rica",
+              description: "Experience the light and fruity notes of Costa Rican coffee, reminiscent of a tropical sunrise in a cup.",
+              roast: "Light",
+              price: 7.99,
+              available: 50,
+              id: v4()
             },
             {
-                name: "Medium Roast 12oz Whole Beans",
-                origin: "Guatemala",
-                description: "Coffee Directly sourced from the Mountains of El Bosque, Santa Cruz Naranjo, Santa Rosa, Guatemala",
-                roast: "Medium",
-                price: 10.99,
-                available: 25,
-                id: v4()
+              name: "Caramel Macchiato",
+              origin: "Brazil",
+              description: "Brazilian medium roast coffee infused with creamy caramel flavors, offering a delightful and sweet coffee experience.",
+              roast: "Medium",
+              price: 10.99,
+              available: 25,
+              id: v4()
             },
             {
-                name: "Strong Dark Roast 12oz Whole Bean",
-                origin: "Guatemala",
-                description:"Coffee Directly sourced from the Mountains of El Bosque, Santa Cruz Naranjo, Santa Rosa, Guatemala",
-                roast: "Dark",
-                price: 11.99,
-                available: 10,
-                id: v4()
+              name: "Mocha Madness",
+              origin: "Ethiopia",
+              description:"Dive into the intense and chocolaty depths of Ethiopian dark roast coffee, a true delight for chocolate and coffee enthusiasts alike.",
+              roast: "Dark",
+              price: 11.99,
+              available: 10,
+              id: v4()
             }
-        ]
+          ]
     };
-}
+  }
 
 handleAddButtonClick = () => { 
-    if (this.state.selectedCoffee != null) {
-      this.setState({
-        formVisibleOnPage: false,
-        selectedCoffee: null,
-        editing: false
+  if (this.state.selectedCoffee != null) {
+    this.setState({
+      formVisibleOnPage: false,
+      selectedCoffee: null,
+      editing: false
+    });
+  } else {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage,
+    }));
+  }
+}
+
+
+  handleAddingNewCoffeeToList = (newCoffee) => {
+    const newMainCoffeeList = this.state.mainCoffeeList.concat(newCoffee);
+    this.setState({mainCoffeeList: newMainCoffeeList, formVisibleOnPage: false });
+  }
+
+  handleChangingSelectedCoffee = (id) => { 
+    const selectedCoffee = this.state.mainCoffeeList.filter(coffee => coffee.id === id)[0];
+    this.setState({selectedCoffee: selectedCoffee});
+}
+
+  handleEditClick = () => {
+    this.setState({editing: true});
+  }
+
+  handleEditingCoffeeInList = (coffeeToEdit) => {
+    const editedMainCoffeeList = this.state.mainCoffeeList
+      .filter(coffee => coffee.id !== this.state.selectedCoffee.id)
+      .concat(coffeeToEdit);
+    this.setState({
+        mainCoffeeList: editedMainCoffeeList,
+        editing: false,
+        selectedCoffee: null
       });
+  }
+
+  handleDeletingCoffee = (id) => {
+    const newMainCoffeeList = this.state.mainCoffeeList.filter(coffee => coffee.id !== id);
+    this.setState({
+      mainCoffeeList: newMainCoffeeList,
+      selectedCoffee: null 
+    });
+  }
+
+  handleSellingCoffee = (id) => {  
+    const {selectedCoffee, mainCoffeeList} = this.state;
+    if (selectedCoffee.available > 0) {
+      selectedCoffee.available -= 1;
     } else {
-      this.setState(prevState => ({
-        formVisibleOnPage: !prevState.formVisibleOnPage,
-      }));
+        selectedCoffee.available = 0;
     }
-  }
-  
-  
-    handleAddingNewCoffeeToList = (newCoffee) => {
-      const newMainCoffeeList = this.state.mainCoffeeList.concat(newCoffee);
-      this.setState({mainCoffeeList: newMainCoffeeList, formVisibleOnPage: false });
-    }
-  
-    handleChangingSelectedCoffee = (id) => { 
-      const selectedCoffee = this.state.mainCoffeeList.filter(coffee => coffee.id === id)[0];
-      this.setState({selectedCoffee: selectedCoffee});
-  }
-  
-    handleEditClick = () => {
-      this.setState({editing: true});
-    }
-  
-    handleEditingCoffeeInList = (coffeeToEdit) => {
-      const editedMainCoffeeList = this.state.mainCoffeeList
-        .filter(coffee => coffee.id !== this.state.selectedCoffee.id)
-        .concat(coffeeToEdit);
-      this.setState({
-          mainCoffeeList: editedMainCoffeeList,
-          editing: false,
-          selectedCoffee: null
-        });
-    }
-  
-    handleDeletingCoffee = (id) => {
-      const newMainCoffeeList = this.state.mainCoffeeList.filter(coffee => coffee.id !== id);
-      this.setState({
+    const index = mainCoffeeList.indexOf(selectedCoffee);
+    const newMainCoffeeList = [...mainCoffeeList];
+    newMainCoffeeList[index] = selectedCoffee;
+    this.setState({
         mainCoffeeList: newMainCoffeeList,
-        selectedCoffee: null 
-      });
+    }); 
+  }
+
+  handleStockingCoffee = (id) => {
+    const {selectedCoffee, mainCoffeeList} = this.state;
+    const index = mainCoffeeList.indexOf(selectedCoffee);
+
+    if (typeof selectedCoffee.available === 'string') {
+      // Convert to a number before incrementing
+      selectedCoffee.available = parseInt(selectedCoffee.available, 10);
     }
-  
-    handleSellingCoffee = (id) => {  
-      const {selectedCoffee, mainCoffeeList} = this.state;
-      if (selectedCoffee.available > 0) {
-        selectedCoffee.available -= 1;
-      } else {
-          selectedCoffee.available = 0;
-      }
-      const index = mainCoffeeList.indexOf(selectedCoffee);
+
+    if (selectedCoffee.available < 130) {
+      selectedCoffee.available += 1;
       const newMainCoffeeList = [...mainCoffeeList];
       newMainCoffeeList[index] = selectedCoffee;
       this.setState({
           mainCoffeeList: newMainCoffeeList,
-      }); 
-    }
-  
-    handleStockingCoffee = (id) => {
-      const {selectedCoffee, mainCoffeeList} = this.state;
-      const index = mainCoffeeList.indexOf(selectedCoffee);
-  
-      if (typeof selectedCoffee.available === 'string') {
-        // Convert to a number before incrementing
-        selectedCoffee.available = parseInt(selectedCoffee.available, 10);
+          });
       }
-  
-      if (selectedCoffee.available < 130) {
-        selectedCoffee.available += 1;
-        const newMainCoffeeList = [...mainCoffeeList];
-        newMainCoffeeList[index] = selectedCoffee;
-        this.setState({
-            mainCoffeeList: newMainCoffeeList,
-            });
-        }
-      }   
-  
-    render(){
-      let currentlyVisibleState = null;
-      let buttonText = "Return to Coffee List";
-  
-      if (this.state.editing ) {      
-        currentlyVisibleState = <EditCoffeeForm coffee = {this.state.selectedCoffee} onEditCoffee = {this.handleEditingCoffeeInList} />
-  
-      } else if (this.state.selectedCoffee != null) {
-        currentlyVisibleState = <CoffeeDetail 
-          coffee = {this.state.selectedCoffee} 
-          onClickingDelete = {this.handleDeletingCoffee}
-          onClickingEdit = {this.handleEditClick} 
-          onCoffeeSale={this.handleSellingCoffee}
-          onCoffeeRestock={this.handleStockingCoffee}
-          />
-      } else if (this.state.formVisibleOnPage) {
-        currentlyVisibleState = <NewCoffeeForm onNewCoffeeCreation={this.handleAddingNewCoffeeToList}/>
-  
-      } else {
-        currentlyVisibleState = <CoffeeList coffeeList={this.state.mainCoffeeList} onCoffeeSelection={this.handleChangingSelectedCoffee} />;
-        buttonText = "Add Coffee";
-      }
-  
-      return (
-        <React.Fragment>
-          <Header onAddButtonClick={this.handleAddButtonClick} buttonText={buttonText}/>
-          {currentlyVisibleState}
-        </React.Fragment>
-      );
+    }   
+
+  render(){
+    let currentlyVisibleState = null;
+    let buttonText = "Return to Coffee List";
+
+    if (this.state.editing ) {      
+      currentlyVisibleState = <EditCoffeeForm coffee = {this.state.selectedCoffee} onEditCoffee = {this.handleEditingCoffeeInList} />
+
+    } else if (this.state.selectedCoffee != null) {
+      currentlyVisibleState = <CoffeeDetail 
+        coffee = {this.state.selectedCoffee} 
+        onClickingDelete = {this.handleDeletingCoffee}
+        onClickingEdit = {this.handleEditClick} 
+        onCoffeeSale={this.handleSellingCoffee}
+        onCoffeeRestock={this.handleStockingCoffee}
+        />
+    } else if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewCoffeeForm onNewCoffeeCreation={this.handleAddingNewCoffeeToList}/>
+
+    } else {
+      currentlyVisibleState = <CoffeeList coffeeList={this.state.mainCoffeeList} onCoffeeSelection={this.handleChangingSelectedCoffee} />;
+      buttonText = "Add Coffee";
     }
+
+    return (
+      <React.Fragment>
+        <Header onAddButtonClick={this.handleAddButtonClick} buttonText={buttonText}/>
+        {currentlyVisibleState}
+      </React.Fragment>
+    );
   }
-  
-  export default CoffeeControl;
+}
+
+export default CoffeeControl;
